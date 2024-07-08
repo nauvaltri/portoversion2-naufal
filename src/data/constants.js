39e6f5825efa0e470a9b5493c1eed6a947cdd28b
nauvaltr <naufaltrisubaktilbb123@gmail.com>
2024-07-08 23:36:39 +0700

    Add initial project files

---
+++ src/data/constants.js
@@ -0,0 +1,373 @@
+import gambar1 from "../images/Silaboev1.png";
+import gambar2 from "../images/Silaboev2.png";
+import gambar3 from "../images/FlexiFun.png";
+import gambar4 from "../images/Revive.png";
+import gambar5 from "../images/Rent Cars.png";
+import gambar6 from "../images/Skin Diary.png";
+import gambar7 from "../images/concert.png";
+import gambar8 from "../images/asakitasama.png";
+import gambar9 from "../images/Porto v1.png";
+import gambar10 from "../images/profile.png";
+
+
+
+
+
+
+
+export const Bio = {
+  name: "Naufal",
+  roles: [
+    "UI/UX Designer",
+    "Front-End Developer",
+    "Sistem Analyst",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/nauvaltri",
+  resume:
+    "https://drive.google.com/file/d/1-TLky2JnEbBd0CVm5FqOe1h62HUScM19/view?usp=sharing",
+  portofolio: "https://drive.google.com/file/d/1Ka3FQ7EuZOL2xuu99PxVIxi3FMHZ4yuH/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/naufaltrisubakti/",
+  twitter: "https://twitter.com/RishavChanda",
+  insta: "https://www.instagram.com/rishav_chanda/",
+  facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+ 
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+      {
+        name: "Laravel",
+        image:
+          " https://upload.wikimedia.org/wikipedia/commons/9/9a/Laravel.svg",
+      },
+      
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+      {
+        name: "Webflow",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/9/92/Webflow_logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Blender",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Blender_logo_no_text.svg/1251px-Blender_logo_no_text.svg.png",
+      },
+     
+    ],
+  },
+
+  // {
+  //   title: "Other",
+  //   skills: [
+      
+      
+  //   ],
+  // },
+  // {
+  //   title: "UI/UX Design",
+  //   skills: [
+
+  //   ],
+  // },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://gemastik.kemdikbud.go.id/assets-frontsite/kompetisi/gemastik/img/logo/logo-gemastik.png",
+    role: " UGM Contingent in Gemastik XVII User Experience Design",
+    company: "Gemastik",
+    date: "May 2024 - Present",
+    desc: "• I perform thorough research, surveys, and interviews to gain a profound understanding of user needs, ensuring that our design process prioritizes user-centricity. In a two-week period, I apply the design thinking approach to develop an application, concentrating on crafting an innovative and functional solution. Furthermore, I conduct extensive research on user experience and consolidate my findings into a comprehensive proposal, illustrating a distinct alignment between user insights and design strategies.",
+    skills: [
+      "Figma",
+      "Draw.Io",
+     ,
+    ],
+    
+  },
+  {
+    id: 1,
+    img: "https://gemastik.kemdikbud.go.id/assets-frontsite/kompetisi/gemastik/img/logo/logo-gemastik.png",
+    role: " UGM Contingent in Gemastik XVII Smart City",
+    company: "Gemastik",
+    date: "May 2024 - Present",
+    desc: "• Building an integrated smart city application platform with IoT technology. This application is developed based on community needs and issues regarding green open spaces in a specific area. The application will integrate with the local government system.",
+    skills: [
+      "Flutter",
+      "Figma",
+      "Dart",
+      "Draw.Io",
+
+
+    ],
+    
+  },
+  {
+    id: 2,
+    img: "http://trpl.sv.ugm.ac.id/wp-content/uploads/sites/1382/2020/12/logo-TRPL.jpg",
+    role: "Front Developer",
+    company: "TRPL Lab",
+    date: "Feb 2024 - June 2024",
+    desc: "• Building a website platform system to assist in managing software engineering technology laboratories more systematically. I developed this platform using Laravel, Tailwind CSS, and JavaScript technologies to make the platform's interface more attractive and interactive.",
+    skills: [
+      "Tailwind CSS",
+      "Laravel",
+      "PHP",
+      "Trello",
+      "Java Script",
+    ],
+    
+  },
+
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://ugm.ac.id/wp-content/uploads/2022/11/LOGO-UGM-BAKU-tnp-back-grou-300x300.jpg",
+    school: "Universitas Gadjah Mada",
+    date: "Aug 2022 - Present",
+    grade: "3.66 GPA",
+    desc: "I am currently pursuing a Bachelor's degree in Applied Software Engineering Technology at Gadjah Mada University, Yogyakarta. I have completed 4 semesters and have a GPA of 3.66. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, Web Development, 2D and 3D Design Applications, Software Testing, and more. Additionally, I am a member of the UGM ICT Student Community, where I engage in exciting projects with a team of talented developers.",
+    degree: "Software Engineering Technology",
+  },
+  {
+    id: 1,
+    img: "https://upload.wikimedia.org/wikipedia/id/thumb/2/26/SMA_Negeri_2_Bantul_logo.png/150px-SMA_Negeri_2_Bantul_logo.png",
+    school: "Senior High School 2 Bantul",
+    date: "Apr 2019 - Apr 2022",
+    grade: "87.25",
+    desc: "I completed my 12th-grade education at SMA N 2 Bantul, Yogyakarta, where I studied natural sciences.",
+    degree: "Natural Sciences",
+  },
+
+];
+
+
+export const projects = [
+  {
+    id: 11,
+    title: "Silaboe V1",
+    date: "2023",
+    description:
+      "Building a laboratory information system to facilitate practical and simple laboratory management. The application consists of a landing page and a dashboard, built using the Laravel framework, with JavaScript for programming and Tailwind CSS for styling. ",
+    image: gambar1,
+    tags: [
+      "Laravel",
+      "Tailwind CSS",
+      "Java Sricpt",
+      "Trello",
+      "Github",
+    ],
+    category: "Web App",
+    github: "https://github.com/nauvaltri/Sillaboe-V1-TRPL",
+    
+  },
+  {
+    id: 9,
+    title: "Silaboe V2",
+    date: "2024",
+    description:
+      "Developing a web-based laboratory information system application version 2 with different design, features, and functionalities. This application utilizes the Laravel framework and Tailwind CSS for styling, presented in an interactive and responsive manner. ",
+      image: gambar2,
+    tags: [
+      "Laravel",
+      "Tailwind CSS",
+      "Postman",
+      "Java Script",
+      "Github",
+
+    ],
+    category: "Web App",
+    github: "https://github.com/nauvaltri/Silaboe-FE-V2.git",
+
+  },
+  {
+    id: 0,
+    title: "FlexiFun",
+    date: "June 2024 - Present",
+    description:
+      "Building a UI/UX design for an application using the design thinking method. This UX research began with our study on stroke patients who feel bored during conventional therapy. Therefore, the idea emerged to create a therapy application using AR technology.",
+    image: gambar3,
+    tags: ["Figma","Draw io"],
+    category: "Design",
+    github: "https://www.figma.com/proto/ikfQc2Q3FdD5ymEOQKQMx7/Portofolio-Asakitasama?page-id=0%3A1&node-id=119-1428&viewport=-4633%2C-612%2C0.43&t=FDGwVz2mpxyiCdlh-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=119%3A1478",
+
+  },
+  {
+    id: 1,
+    title: "Revive",
+    date: "June 2024 - Present",
+    description:
+      "Development of an application system for the smart city of Bandung. This application development is based on issues in Bandung's Green Open Spaces. We created the UX design and system architecture. This system will be integrated with IoT and external APIs. The development utilizes Figma and Flutter tools. ",
+    image: gambar4,
+    tags: [
+      "Figma",
+      "Draw.io",
+      "Flutter",
+
+    ],
+    category: "web app",
+    github: "https://www.figma.com/proto/GkaseRkz9YWlwyUEw6fSgo/Untitled?page-id=0%3A1&node-id=1-1436&viewport=1730%2C2567%2C0.75&t=ET9wLJK6oqmQMvEp-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=17%3A681&show-proto-sidebar=1",
+  
+  },
+  {
+    id: 2,
+    title: "Car Rental",
+    date: "2024",
+    description:
+      "Building the UI/UX for a car rental application. This design follows the principles of simplicity and elegance. The development uses Figma software, creating an interactive and engaging interface.",
+    image: gambar5,
+    tags: ["Figma",
+      ],
+    category: "Design",
+    github: "https://www.figma.com/proto/zNnCQX6hcjPDkDNmxuZ6Ig/rental-Car-Mobile?page-id=0%3A1&node-id=1-10&viewport=630%2C515%2C0.23&t=8hveUHZnuvRFW6VM-1&scaling=scale-down&content-scaling=fixed",
+  
+
+  },
+  {
+    id: 3,
+    title: "Skin Diary",
+    date: "2023",
+    description:
+      "Building a UI/UX design for a mobile platform that helps users find the right skincare products for their skin type and needs. This application uses AI to scan skin types and provide suitable skincare recommendations.",
+    image: gambar6,
+      
+    tags: ["Figma","Canva"],
+    category: "android app",
+    github: "https://www.figma.com/proto/ybj34xwjnWC9wc6h2O7Mpn/SkinDiary?page-id=0%3A1&node-id=5-5462&viewport=45%2C1390%2C0.29&t=nizXyxKhe9FZBKjq-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=5%3A5462",
+
+  },
+
+  { 
+    id: 4,
+    title: "Charity Concert ",
+    date: "2024",
+    description:
+      "As a UI and UX research designer, I created a platform to facilitate users who want to donate to natural disaster victims by simply purchasing concert tickets. The landing page is designed to be as attractive as possible, with components placed according to user experience principles.Additionally, this design has been made responsive for desktop, tablet, and mobile devices.",
+    image: gambar7,
+     
+    tags: ["Figma"],
+    category: "Desain",
+    github: "https://www.figma.com/design/JcwxE7h330nsdDioqzjdzz/UI%2FUX-Web-Design-Suidmedia?node-id=0-1&t=uBH30Vh9pMfGAutG-1",
+    
+  },
+  {
+    id: 5,
+    title: "Asakitasama",
+    date: "2023",
+    description:
+      "Building a portfolio design as an IT services platform. This website platform consists only of a landing page without a back end.This design has been made responsive for desktop, tablet, and mobile devices",
+    image: gambar8,
+    tags: ["Figma"],
+    category: "Design",
+    github: "https://www.figma.com/design/6EJOEQqdop2dNYNEBFSNRd/Portofolio-Asakitasama?node-id=0-1&t=gdwvoXTBJKkYhAvB-1",
+    
+  },
+  {
+    id: 6,
+    title: "Profile Web",
+    date: "2023",
+    description:
+      "Building an interactive and responsive front-end personal profile using HTML, JavaScript, and CSS.",
+    image: gambar10,
+      
+    tags: ["HTML","Css","Java Script","Figma","Github"],
+    category: "web app ",
+    github: "https://github.com/nauvaltri/Profilekuu.git",
+  },
+  {
+    id: 7,
+    title: "Portofolio Versi 1",
+    date: "2023",
+    description:
+      "Building a personal portfolio website platform version one using the React JS framework and CSS for styling.",
+    image: gambar9,
+      
+    tags: ["React Js","CSS"],
+    category: "android app",
+    github: "https://github.com/nauvaltri/portonaufaltrisubakti.git",
+    
+  },
+  {
+    id: 8,
+    title: "Portofolio Versi 2",
+    date: "2024",
+    description:
+      "Building a personal portfolio website platform version two using the Next JS framework and CSS for styling.This portfolio presents various kinds of information about myself",
+    image:
+      "https://dontrepeatyourself.org/media/face-recognition-with-python-dlib-and-deep-learning_cezKZBj.png",
+    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    category: "Design",
+    github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+
+  },
+];
